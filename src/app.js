1844b5aadaef42448ff02a1efdd14d7a7988bfc5
mauriciogc <mauriciogc@gmail.com>
2024-09-25 10:51:48 -0600

    Connecting the API with mongoose to store the data permanently.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { mongoose, Types: { ObjectId } } = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,22 +11,45 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Importar las rutas
-const messageRoutes = require('./routes/messages');
+const urlDB = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017';
 
-// Montamos las rutas para "/messages"
-app.use('/messages', messageRoutes);
+async function connectToDatabase() {
+  try {
+    //Conecta a mongo y a la base
+    await mongoose.connect(urlDB);
+    console.log('Conectado a la base de datos con mongoose');
+    mongoose.connection.useDb(process.env.DATABASE || 'blog-messages');
+    console.log(`Usando la base de datos`);
 
-// Middleware de manejo de errores centralizado
-app.use((err, req, res, next) => {
-  console.error(err.stack); // Mostrar el error en la consola
-  res.status(err.status || 500).json({
-    message: err.message || "Error interno del servidor",
-    error: process.env.NODE_ENV === "production" ? {} : err, // No exponer detalles de error en producción
-  });
-});
+    // Importar el modelo de Message
+    const Message = require('./models/message');
 
-//-- Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+    // Middleware para inyectar el modelo en las rutas
+    app.use((req, res, next) => {
+      req.context = { models: { Message }, ObjectId };
+      next();
+    });
+
+    // Importar las rutas de mensajes
+    const messageRoutes = require('./routes/messages');
+    app.use('/messages', messageRoutes);
+
+    // Middleware de manejo de errores centralizado
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(err.status || 500).json({
+        message: err.message || 'Error interno del servidor',
+        error: process.env.NODE_ENV === 'production' ? {} : err,
+      });
+    });
+
+    // Iniciar el servidor
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+  }
+}
+
+connectToDatabase();
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,63 +1,119 @@
 const express = require('express');
 const router = express.Router();
 
-// Simulación de base de datos de mensajes
-const messages = [];
-
 router.use((req, res, next) => {
   console.log(`Request method: ${req.method}, Request URL: ${req.originalUrl}`);
   next();
 });
 
 // Obtener todos los mensajes
-router.get('/', (req, res) => {
-  return res.json(messages);
+router.get('/', async (req, res, next) => {
+  try {
+    const messages = await req.context.models.Message.find();
+    return res.json(messages);
+  } catch (er) {
+    const error = new Error(
+      'Error al obtener los mensajes de la base de datos'
+    );
+    error.status = 500;
+    return next(error);
+  }
 });
 
 // Obtener un mensaje específico por ID
-router.get('/:messageId', (req, res, next) => {
+router.get('/:messageId', async (req, res, next) => {
   const { messageId } = req.params;
-  const message = messages.find((msg) => messageId === msg.id);
+  try {
+    if (!req.context.ObjectId.isValid(messageId)) {
+      const error = new Error(`ID ${messageId} no es válido`);
+      error.status = 400;
+      return next(error);
+    }
 
-  if (!message) {
-    const error = new Error('Mensaje no encontrado.');
-    error.status = 404;
+    const myid = new req.context.ObjectId(messageId);
+    const message = await req.context.models.Message.findOne({
+      _id: myid,
+    });
 
-    //Para el error al middleware general de manejo de errores
+    console.log('---',message)
+
+    if (!message) {
+      const error = new Error(`Mensaje con ID ${messageId} no encontrado`);
+      error.status = 404;
+      return next(error);
+    }
+
+    return res.json(message);
+  } catch (er) {
+    const error = new Error(
+      `Error al obtener el mensaje con ID ${messageId} desde la base de datos`
+    );
+    error.status = 500;
     return next(error);
   }
-
-  return res.json(message);
 });
 
 // Agregar un nuevo mensaje
-router.post('/', (req, res, next) => {
-  const { message } = req.body;
-  if (!message) {
-    const error = new Error('No se ha encontrado la propiedad message');
-    error.status = 404;
-    //Para el error al middleware general de manejo de errores
+router.post('/', async (req, res, next) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      const error = new Error('La propiedad "message" es requerida');
+      error.status = 404;
+      return next(error);
+    }
+
+    // Crear el nuevo mensaje
+    const newMessage = new req.context.models.Message({ message });
+    const savedMessage = await newMessage.save();
+
+    // Validar si la inserción fue exitosa
+    if (!savedMessage) {
+      const error = new Error('Error al guardar el mensaje');
+      error.status = 500;
+      return next(error);
+    }
+
+    return res.status(201).json(savedMessage);
+  } catch (er) {
+    const error = new Error('Error al agregar el mensaje a la base de datos');
+    error.status = 500;
     return next(error);
   }
-
-  const newMessage = { id: `${messages.length + 1}`, message };
-  messages.push(newMessage);
-  return res.status(201).json(newMessage);
 });
 
 // Eliminar un mensaje por ID
-router.delete('/:messageId', (req, res, next) => {
+router.delete('/:messageId', async (req, res, next) => {
   const { messageId } = req.params;
-  const index = messages.findIndex((msg) => msg.id === messageId);
-  if (index === -1) {
-    const error = new Error('Mensaje no encontrado.');
-    error.status = 404;
-    //Para el error al middleware general de manejo de errores
-    return next(error);
-  }
 
-  messages.splice(index, 1);
-  return res.send(`Mensaje con ID ${messageId} eliminado.`);
+  try {
+    if (!req.context.ObjectId.isValid(messageId)) {
+      const error = new Error(`ID ${messageId} no es válido`);
+      error.status = 400;
+      return next(error);
+    }
+
+    const myid = new req.context.ObjectId(messageId);
+    // Buscar y eliminar el mensaje
+    const result = await req.context.models.Message.deleteOne({
+      _id: myid,
+    });
+
+    if (result.deletedCount === 0) {
+      const error = new Error(`Mensaje con ID ${messageId} no encontrado`);
+      error.status = 404;
+      return next(error);
+    }
+
+    return res.send(`Mensaje con ID ${messageId} eliminado.`);
+  } catch (err) {
+    return next(
+      new Error(
+        `Error al eliminar el mensaje de la base de datos con ID ${messageId}`
+      )
+    );
+  }
 });
 
 module.exports = router;
